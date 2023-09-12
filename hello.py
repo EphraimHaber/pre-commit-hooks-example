9fb9930a100ceb6e1e0b2e2344defd8bd3ebd3c7
EphraimHaber <ephraimxyz@gmail.com>
2023-09-12 23:52:12 +0300

    added mypy

---
+++ hello.py
@@ -0,0 +1,26 @@
+"""
+def greet(name:str):
+    print(f"hello, {name}")
+    print("nice to meet you!")
+
+def main():
+    for name in "James","Subscriber","other":
+        greet(name)
+
+if __name__ == "__main__":
+    main()
+"""
+
+
+def greet(name: str):
+    print(f"hello, {name}")
+    print("nice to meet you!")
+
+
+def main():
+    for name in "James", "Subscriber", "other":
+        greet(name)
+
+
+if __name__ == "__main__":
+    main()
